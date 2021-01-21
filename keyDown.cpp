683927e6af6a43d60a9244e1e82d3d017e9c9f2c
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-01-21 14:56:31 +0900

    keyDown

---
+++ keyDown.cpp
@@ -0,0 +1,88 @@
+﻿
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("Class");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	static int x = 100;
+	static int y = 100;
+	static bool sd = true;
+	static TCHAR word[] = TEXT("A"); 
+
+	switch (iMessage) {
+	case WM_KEYDOWN:
+		switch (wParam){
+		case VK_SPACE:
+			if (sd == false){
+				word[0] = TEXT('#');
+				sd = true;
+			}
+			else if (sd == true){
+				word[0] = TEXT('A');
+				sd = false;
+			}
+			break;
+
+		case VK_LEFT:
+			x -= 8;
+			break;
+		case VK_RIGHT:
+			x += 8;
+			break;
+		case VK_UP:
+			y -= 8;
+			break;
+		case VK_DOWN:
+			y += 8;
+			break;
+		}
+		InvalidateRect(hWnd, NULL, FALSE);
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		TextOut(hdc, x, y, word, 1);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
