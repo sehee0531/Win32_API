bcc639d2ee96652796a7f930cfc5f74052924ea5
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-01-26 17:14:20 +0900

    8장_EX

---
+++ About/About.cpp
@@ -0,0 +1,71 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass=TEXT("About");
+
+int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance
+	  ,LPSTR lpszCmdParam,int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst=hInstance;
+	
+	WndClass.cbClsExtra=0;
+	WndClass.cbWndExtra=0;
+	WndClass.hbrBackground=(HBRUSH)(COLOR_WINDOW+1);
+	WndClass.hCursor=LoadCursor(NULL,IDC_ARROW);
+	WndClass.hIcon=LoadIcon(NULL,IDI_APPLICATION);
+	WndClass.hInstance=hInstance;
+	WndClass.lpfnWndProc=WndProc;
+	WndClass.lpszClassName=lpszClass;
+	WndClass.lpszMenuName=NULL;
+	WndClass.style=CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd=CreateWindow(lpszClass,lpszClass,WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,
+		NULL,(HMENU)NULL,hInstance,NULL);
+	ShowWindow(hWnd,nCmdShow);
+	
+	while (GetMessage(&Message,NULL,0,0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+#include "resource.h"
+BOOL CALLBACK AboutDlgProc(HWND hDlg,UINT iMessage,WPARAM wParam,LPARAM lParam)
+{
+	switch (iMessage) {
+	case WM_INITDIALOG:
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+		case IDOK:
+			EndDialog(hDlg,IDOK);
+			return TRUE;
+		case IDCANCEL:
+			EndDialog(hDlg,IDCANCEL);
+			return TRUE;
+		}
+		break;
+	}
+	return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
+{
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+		DialogBox(g_hInst,MAKEINTRESOURCE(IDD_DIALOG1),hWnd,AboutDlgProc);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd,iMessage,wParam,lParam));
+}
